2687d47a68d0c997478e3a4192d287365fa64824
Gabriel Pereira <140173310+adotive@users.noreply.github.com>
2023-09-24 19:17:26 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo pacman -Sy firefox zsh waybar hyprland neovim kitty
+git clone https://github.com/adotive/kitty ~/.config/kitty
+git clone https://github.com/adotive/waybar ~/.config/waybar
+mkdir pix 
+mkdir dos
+curl -sS https://starship.rs/install.sh | sh
+git clone https://aur.archlinux.org/yay.git ~/.config/yay
+cd .config/yay
+makepkg -si
+cd 
+sudo rm -r arch
